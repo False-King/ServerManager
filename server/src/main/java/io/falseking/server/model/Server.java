f79a816ebd24731ba8a67374045f4d7fe855645f
False-King <tu.marcelo2412@gmail.com>
2023-09-22 13:36:06 -0300

    Initial Commit

---
+++ server/src/main/java/io/falseking/server/model/Server.java
@@ -0,0 +1,12 @@
+package io.falseking.server.model;
+
+import io.falseking.server.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAdress;
+    private String name;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
